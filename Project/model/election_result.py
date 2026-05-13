548f6d9182c861fc10a0928d28f0fd060ead060e
neojarvis <techadmin@iamneo.ai>
2026-05-13 17:48:14 +0000

    Manual-save-commit-5/13/2026--11:18:13-PM

--- Project/model/election_result.py
+++ Project/model/election_result.py
@@ -1,1 +1,11 @@
-....
+class ElectionResult:
+    def __init__(self,result_id=0,election_id,candidate_id=0,total_votes=0,result_status="PENDING"):
+        if total_votes<0:
+            raise ValueError("Votes cannot be negative")
+        self.result_id=result_id
+        self.election_id=election_id
+        self.candidate_id=candidate_id
+        self.total_votes=total_votes
+        self.result_status=result_status
+    def __str__(self):
+        return f"{self.result_id} {self.result_status}"
