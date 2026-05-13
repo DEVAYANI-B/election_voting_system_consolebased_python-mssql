bc4bbf86ea1c9196fbe8bdc6c9e79e6b1914a6bf
neojarvis <techadmin@iamneo.ai>
2026-05-13 17:42:40 +0000

    Manual-save-commit-5/13/2026--11:12:36-PM

--- Project/model/vote.py
+++ Project/model/vote.py
@@ -1,1 +1,13 @@
-.....
+class Vote:
+    def __init__(self,vote_id=0,election_id=0,voter_id=0,candidate_id=0,vote_date=""):
+        if election_id<=0 or \ voter_id<=0 or \ candidate_id<=0:
+            raise ValueError(
+                "IDs must be greater than 0"
+            )
+        self.vote_id=vote_id
+        self.election_id=election_id
+        self.voter_id=voter_id
+        self.candidate_id=candidate_id
+        self.vote_date=vote_date
+    def __str__(self):
+        return f"{self.vote_id}"
