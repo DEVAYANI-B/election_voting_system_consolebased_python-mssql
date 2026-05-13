4d9e5255c0425e12f6cf0038b75ae9df6bebe913
neojarvis <techadmin@iamneo.ai>
2026-05-13 17:34:22 +0000

    Manual-save-commit-5/13/2026--11:04:19-PM

---
+++ Project/model/voter.py
@@ -0,0 +1,10 @@
+class Voter:
+    def __init__(self,voter_id=0,name="",age=0,gender="",phone="",constituency=""):
+        self.voter_id=voter_id
+        self.name=name
+        self.age=age
+        self.gender=gender
+        self.phone=phone
+        self.constituency=constituency
+    def __str__(self):
+        return f"{self.voter_id} {self.name}"
