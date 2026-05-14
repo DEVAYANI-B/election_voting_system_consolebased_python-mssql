0aea26041406e6d5670b2ea5514c33b80fadbb5a
neojarvis <techadmin@iamneo.ai>
2026-05-14 13:41:24 +0000

    Manual-save-commit-5/14/2026--7:11:20-PM

--- Project/exception/candidate_not_found_exception.py
+++ Project/exception/candidate_not_found_exception.py
@@ -1,3 +1,7 @@
 class CandidateNotFoundException(Exception):
-    pass
+    def __init__(self,message="Candidate not found"):
+        super().__init__(message)
+        self.message=message
+    def __str__(self):
+        return self.message
     
