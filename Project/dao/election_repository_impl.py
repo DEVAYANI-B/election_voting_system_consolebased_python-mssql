1fc2496e224b0d44e4c14dc84b8221564ff1dd86
neojarvis <techadmin@iamneo.ai>
2026-05-14 14:01:04 +0000

    Auto-save-commit-5/14/2026--7:31:03-PM

--- Project/dao/election_repository_impl.py
+++ Project/dao/election_repository_impl.py
@@ -1,3 +1,28 @@
 from typing import List
 from dao.election_repository import ElectionRepository
-from model
+from model.candidate import Candidate
+from model.voter import Voter
+from model.election import Election
+from model.vote import Vote
+from model.elecction_result import ElectionResult
+from util.db_connection import DBConnection
+from exception.candidate_not_found_exception import CandidateNotFoundException
+from exception.voter_not_found_exception import VoterNotFoundException
+from exception.invalid_vote_exception import InvalidVoteException
+from exception.election_closure_exception import ElectionClosureException
+
+class EelectionRepositoryImpl(ElectionRepository):
+    def __init__(self):
+        self.conn=DBConnection.get_connection()
+        self.cursor=self.conn.cursor()
+    def add_candidate(self,candidate: Candidate)->bool:
+        try:
+            self.cursor.execute("INSERT INTO Candidates (name,age,gender,party,constituency) VALUES (?,?,?,?,?)",
+            (candidate.name,candidate.age,candidate.gender,candidate.party,candidate.constituency)
+            )
+            self.conn.commit()
+            return True
+        except Exception:
+            return False
+    def update_candidate(self,candidate: Candidate)->bool:
+        self.get_candidate_by_id()
