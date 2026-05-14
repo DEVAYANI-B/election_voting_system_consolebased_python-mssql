d268942296fe682410bdee34a79074294afaa2ea
neojarvis <techadmin@iamneo.ai>
2026-05-14 07:58:43 +0000

    Manual-save-commit-5/14/2026--1:28:41-PM

--- Project/dao/election_repository.py
+++ Project/dao/election_repository.py
@@ -1,25 +1,46 @@
 from abc import ABC, abstractmethod
+from typing import List
+from model.candidate import Candidate
+from model.voter import Voter
+from model.election import Election
+from model.vote import Vote
+from model.election_result import ElectionResult
+
 
 class ElectionRepository(ABC):
     @abstractmethod
-    def add_candidate(self,candidate):
+    def add_candidate(self,candidate: Candidate)-> bool:
         pass
     @abstractmethod
-    def get_candidate_by_id(self,candidate_id):
+    def update_candidate(self,candidate:Candidate)->bool:
         pass
     @abstractmethod
-    def add_voter(self,voter):
+    def delete_candidate(self,candidate_id: int)->bool:
+        pass
+
+    @abstractmethod
+    def get_candidate_by_id(self,candidate_id: int)-> Candidate:
         pass
     @abstractmethod
-    def get_voter_by_id(self,voter_id):
+    def add_voter(self,voter: Voter)->bool:
+        pass
+    @abstractmethod
+    def update_voter(self,voter:Voter)->bool:
+        pass
+    @abstractmethod
+    def delete_voter(self,voter_id:int)->bool:
+        pass
+    @abstractmethod
+    def get_voter_by_id(self,voter_id:int)->Voter:
         pass
     @abstractmethod
     def add_election(self,election):
         pass
+    
     @abstractmethod
     def cast_vote(self,vote):
         pass
     @abstractmethod
-    def declar_election_result(self,result):
+    def declare_election_result(self,result):
         pass
         
