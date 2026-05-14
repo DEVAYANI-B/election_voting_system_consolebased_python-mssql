eed9571fb1c54c3a4b4763d66894ae01bcf57558
neojarvis <techadmin@iamneo.ai>
2026-05-14 13:42:55 +0000

    Manual-save-commit-5/14/2026--7:12:54-PM

--- Project/exception/election_closure_exception.py
+++ Project/exception/election_closure_exception.py
@@ -1,2 +1,6 @@
 class ElectionClosureException(Exception):
-    pass
+    def __init__(self,message="Election closure error"):
+        super().__init__(message)
+        self.message=message
+    def __str__(self):
+        return self.message
